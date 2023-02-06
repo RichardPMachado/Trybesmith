669478418c97d47dfe2894d7974a8407e5b13356
RichardPMachado <richardmachado2211@gmail.com>
2023-02-06 19:01:49 -0300

    feat[requisito 01] configuração de interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import productsRouter from './Routes/products.router';
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Tudo certo!');
+});
 
 export default app;
