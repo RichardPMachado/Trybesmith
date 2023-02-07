3cc5c7932847aabd8a996656a57fd81aa366d8fe
RichardPMachado <richardmachado2211@gmail.com>
2023-02-07 18:30:08 -0300

    feat[requisito 2] criando a Products router

--- src/Routes/products.router.ts
+++ src/Routes/products.router.ts
@@ -3,6 +3,7 @@ import * as ProductsController from '../controllers/Products.controller';
 
 const productsRouter = Router();
 
+productsRouter.get('/', (req, res) => ProductsController.getAllProducts(req, res));
 productsRouter.post('/', (req, res) => ProductsController.createProduct(req, res));
 
 export default productsRouter;
