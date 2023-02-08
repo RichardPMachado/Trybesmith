ffc577969bb959382648ffa73a5d818d37bee366
RichardPMachado <richardmachado2211@gmail.com>
2023-02-07 23:18:32 -0300

    feat[requisito 4] completo

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import ordersRouter from './Routes/Orders.router';
 import productsRouter from './Routes/products.router';
 import usersRouter from './Routes/users.router';
 
@@ -8,6 +9,7 @@ app.use(express.json());
 
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
+app.use('/orders', ordersRouter);
 
 app.get('/', (_req, res) => {
   res.status(200).send('Tudo certo!');
