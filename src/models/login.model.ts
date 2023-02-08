fc94c5f4ec1453ed21fa69faa12f54b468556b8b
RichardPMachado <richardmachado2211@gmail.com>
2023-02-08 01:17:36 -0300

    feat[requisito 5] configurando login model

---
+++ src/models/login.model.ts
@@ -0,0 +1,14 @@
+import { IUser } from '../interfaces';
+import connection from './connection';
+
+async function findByNameAndPassword(username: string, password: string) {
+  const query = 'SELECT * FROM Trybesmith.users WHERE username = ? AND password = ?';
+  const value = [username, password];
+  const [result] = await connection.execute(query, value);
+  const user = result as IUser[];
+  return user;
+}
+
+export default {
+  findByNameAndPassword,
+};
